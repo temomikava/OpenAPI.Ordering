971f8995323315274234d2ebedad578303ae53fa
Temur Mikava <temsonmikava@gmail.com>
2024-06-27 20:44:15 +0400

    Add migration

--- OpenAPI.Ordering/OpenAPI.Ordering/Migrations/20240627164341_AddBackgroundOrderComputationService.Designer.cs
+++ OpenAPI.Ordering/OpenAPI.Ordering/Migrations/20240627164341_AddBackgroundOrderComputationService.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OpenAPI.Identity.Data;
@@ -11,9 +12,10 @@ using OpenAPI.Identity.Data;
 namespace OpenAPI.Ordering.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240627164341_AddBackgroundOrderComputationService")]
+    partial class AddBackgroundOrderComputationService
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OpenAPI.Ordering/OpenAPI.Ordering/Migrations/20240627164341_AddBackgroundOrderComputationService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace OpenAPI.Ordering.Migrations
+{
+    public partial class AddBackgroundOrderComputationService : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ComputationResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TaskId = table.Column<string>(type: "text", nullable: false),
+                    Result = table.Column<decimal>(type: "numeric", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComputationResults", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ComputationResults");
+        }
+    }
+}
